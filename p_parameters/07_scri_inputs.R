3e466836a63a485500a7da619318eb2690db6dfb
svetlanabelitser <93388849+svetlanabelitser@users.noreply.github.com>
2021-11-17 14:32:36 +0100

    Add files via upload

---
+++ p_parameters/07_scri_inputs.R
@@ -0,0 +1,30 @@
+# Program Information  ----------------------------------------------------
+
+# Program:      07_scri_inputs.R 
+# Author:       Anna Schultze, Svetlana Belitser; Ema Alsina, Sophie Bots, Ivonne Martens 
+# Description:  sets a series of "parameters" (i.e, input values) 
+# Requirements: none
+#
+# INPUT LISTS  -----------------------------------------------------------
+
+
+# package list 
+packages<-  c( "survival" )   
+#Sv packages<- c("tidyverse", "data.table", "here", "SCCS", "gtsummary", "janitor", "ggplot2")
+
+# functions list:
+func_names <- c("standardsccs2.R", "formatdata2.R", "table1.R")
+func_dir   <- paste0(getwd(),"/p_steps/")
+
+
+# names of input data 
+raw_data <- "D3_study_variables_for_SCRI.RData"
+raw_data_name <- "D3_study_variables_for_SCRI"
+intermediate_data <- "scri_data_extract"
+
+
+
+
+
+
+
