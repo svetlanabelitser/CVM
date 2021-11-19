0bf4e7c27250fec8cf787a8f855ab077572ab470
svetlanabelitser <93388849+svetlanabelitser@users.noreply.github.com>
2021-11-19 14:26:59 +0100

    Update table1.R

--- p_steps/table1.R
+++ p_steps/table1.R
@@ -5,12 +5,19 @@
 # Author:       Svetlana Belitser 
 
 
-table1 <- function(x, title="", print=T, digits=2){
-  x <- as.factor(x)
+table1 <- function(x, title="", digits=2, sep=" & ", print=T){
+  if(length(dim(x))==2){
+    for(icol in 2:ncol(x)) x[,1] <- paste(x[,1],x[,icol],sep=sep)
+    x <- x[,1]
+  }  
+  else x <- as.factor(x)
   if(print & title!="") cat(paste(title,"\n"))
   cbind( n=(tb<-table(x,useNA="ifany")), 
          percent=round(100*tb/sum(tb),digits), 
-         percent2=c(round(100*(tb2<-table(x))/sum(tb2),digits),rep(NA,length(tb)-length(tb2))) )[,c(T,T,any(is.na(x)))]
+         cum_percent=round(100*cumsum( tb/sum(tb) ),digits), 
+         percent2=c(round(100*(tb2<-table(x))/sum(tb2),digits),rep(NA,length(tb)-length(tb2))),
+         cum_percent2 = c(round(100*cumsum( (tb2<-table(x))/sum(tb2) ),digits),rep(NA,length(tb)-length(tb2))) 
+         )[,c(T,T,T,any(is.na(x)),any(is.na(x)))]
 }
 #
 #table1 <- function(x, digits=2){
