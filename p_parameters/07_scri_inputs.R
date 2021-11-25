0dc5884742f611480356ab3269cb111c6c0ea657
svetlanabelitser <93388849+svetlanabelitser@users.noreply.github.com>
2021-11-25 07:26:56 +0100

    Add files via upload

--- p_parameters/07_scri_inputs.R
+++ p_parameters/07_scri_inputs.R
@@ -12,8 +12,8 @@
 packages<-  c( "survival" )   
 
 # functions list:
-func_names <- c("standardsccs2.R", "formatdata2.R", "table1.R")
-func_dir   <- paste0(getwd(),"/p_steps/")
+func_names <- c("scri.R", "formatdata2.R", "table1.R")
+func_dir   <- paste0(thisdir,"/p_steps/")
 
 
 # names of input data 
@@ -22,7 +22,7 @@ raw_data_name <- "D3_study_variables_for_SCRI"
 intermediate_data <- "scri_data_extract"
 
 # age categories
-age_cat <- c(-1,18,24,29,39,49,55,65,80,120)
+age_cat <- c(-1,30,120)
 
 
 
