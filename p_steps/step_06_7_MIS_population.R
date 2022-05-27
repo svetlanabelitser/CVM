9db3d5f248bb52602c0e5420510bd7e7951447cc
Davide <davide.messina@ars.toscana.it>
2022-05-27 19:14:35 +0200

    Create long df and added new covid df "D3_outcomes_severity_episodes_covid"

--- p_steps/step_06_7_MIS_population.R
+++ p_steps/step_06_7_MIS_population.R
@@ -12,23 +12,23 @@ for (subpop in subpopulations_non_empty) {
   first_jan_2021<-as_date("20210101")
   
   load(paste0(dirtemp,"D3_events_ALL_OUTCOMES",suffix[[subpop]],".RData"))
-  load(paste0(dirtemp,"D3_covid_episodes",suffix[[subpop]],".RData")) #L1plus
+  load(paste0(dirtemp,"D3_outcomes_severity_episodes_covid",suffix[[subpop]],".RData")) #L1plus
   load(paste0(dirtemp,"D3_study_population",suffix[[subpop]],".RData"))
   
   events_ALL_OUTCOMES<-get(paste0("D3_events_ALL_OUTCOMES", suffix[[subpop]]))
   rm(list=paste0("D3_events_ALL_OUTCOMES", suffix[[subpop]]))
-  outcomes_covid<-get(paste0("D3_covid_episodes", suffix[[subpop]]))
-  rm(list=paste0("D3_covid_episodes", suffix[[subpop]]))
+  outcomes_covid<-get(paste0("D3_outcomes_severity_episodes_covid", suffix[[subpop]]))
+  rm(list=paste0("D3_outcomes_severity_episodes_covid", suffix[[subpop]]))
   study_population<-get(paste0("D3_study_population", suffix[[subpop]]))
   rm(list=paste0("D3_study_population", suffix[[subpop]]))
   
   #add date of first covid to the population
-  covid_L1<-unique(outcomes_covid[, .(person_id,date)])
-  covid_L1<-covid_L1[,min(date,na.rm = T),by="person_id"]
+  covid_L1<-unique(outcomes_covid[, .(person_id,date_event)])
+  covid_L1<-covid_L1[,min(date_event,na.rm = T),by="person_id"]
   
   setnames(covid_L1,"V1","covid_date")
   D3_study_variables_for_MIS <- merge(study_population, covid_L1, all.x = T, by="person_id")
-  rm(covid_L1)
+  rm(covid_L1, study_population)
   
   tempname<-paste0("D3_study_variables_for_MIS",suffix[[subpop]])
   assign(tempname,D3_study_variables_for_MIS)
@@ -123,20 +123,30 @@ for (subpop in subpopulations_non_empty) {
   #add the study exit date for MIS
   #D3_study_variables_for_MIS[covid_date>date_vax1,study_exit_date_MIS_d:=min(covid_date,study_exit_date,na.rm = T)]
   D4_population_d[covid_date<=date_vax1,history_covid:=1][covid_date>date_vax1 | is.na(covid_date),history_covid:=0]
-  D4_population_d[covid_date>date_vax1 & !is.na(covid_date),study_exit_date_MIS_d:=min(covid_date-1,study_end,study_exit_date),by="person_id"]
+  D4_population_d[covid_date>date_vax1 & !is.na(covid_date),study_exit_date_MIS_d:=min(study_end,study_exit_date),by="person_id"]
   D4_population_d[covid_date<date_vax1 | is.na(covid_date),study_exit_date_MIS_d:=min(study_end,study_exit_date),by="person_id"]
   
   D4_population_d<-D4_population_d[study_exit_date_MIS_d > study_entry_date_MIS_d, ]
   D4_population_d<-D4_population_d[, covid_date := NULL]
   
-  D4_population_d_28gg <- copy(D4_population_d)[,.(person_id, sex, age_at_date_vax_1, ageband_at_date_vax_1, study_entry_date_vax1,
-                             study_exit_date_vax1, study_entry_date_vax2, study_exit_date_vax2,
-                             history_covid, type_vax_1, type_vax_2)]
+  D4_population_d_28gg <- copy(D4_population_d)
+  D4_population_d_28gg <- D4_population_d_28gg[study_exit_date_vax1 > study_exit_date_MIS_d,
+                                               c("study_exit_date_vax1", "study_exit_date_vax2") := list(study_exit_date_MIS_d, NA)]
+  D4_population_d_28gg <- D4_population_d_28gg[study_entry_date_vax2 > study_exit_date_MIS_d, study_entry_date_vax2 := NA]
+  D4_population_d_28gg <- D4_population_d_28gg[study_exit_date_vax2 > study_exit_date_MIS_d, study_exit_date_vax2 := study_exit_date_MIS_d]
+  
+  D4_population_d_28gg[, c("study_entry_date", "study_exit_date") := NULL]
   colA = paste("study_entry_date_vax", 1:2, sep = "")
   colB = paste("study_exit_date_vax", 1:2, sep = "")
   colC = paste("type_vax", 1:2, sep = "_")
   D4_population_d_28gg <- data.table::melt(D4_population_d_28gg, measure = list(colA, colB, colC), variable.name = "Dose",
                            value.name = c("study_entry_date", "study_exit_date", "type_vax"), na.rm = T)
+  
+  study_population <- copy(D4_population_d_28gg)
+  
+  D4_population_d_28gg <- D4_population_d_28gg[,.(person_id, sex, age_at_date_vax_1, ageband_at_date_vax_1, study_entry_date,
+                                                  study_exit_date,  history_covid, type_vax, Dose)]
+  
   D4_population_d_28gg <- D4_population_d_28gg[, days_28 := study_entry_date + 27]
   D4_population_d_28gg <- D4_population_d_28gg[study_exit_date > days_28 & Dose == 1, study_exit_date := days_28]
   D4_population_d_28gg <- D4_population_d_28gg[, days_28 := NULL][, Dose := as.character(Dose)]
@@ -148,6 +158,64 @@ for (subpop in subpopulations_non_empty) {
   
   rm(D4_population_d_28gg)
   rm(list=tempname)
+  
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+
+  
+  setnames(study_population,
+           c("CV_at_date_vax_1", "COVCANCER_at_date_vax_1", "COVCOPD_at_date_vax_1", "COVHIV_at_date_vax_1",
+             "COVCKD_at_date_vax_1", "COVDIAB_at_date_vax_1", "COVOBES_at_date_vax_1", "COVSICKLE_at_date_vax_1",
+             "immunosuppressants_at_date_vax_1", "at_risk_at_date_vax_1"),
+           c("CV", "COVCANCER", "COVCOPD", "COVHIV", "COVCKD", "COVDIAB", "COVOBES", "COVSICKLE",
+             "IMMUNOSUPPR", "any_risk_factors"))
+
+  # study_population <- study_population %>%
+  #   select(person_id, sex, study_entry_date, study_exit_date, ageband_at_date_vax_1, type_vax, Dose, CV, COVCANCER,
+  #          COVCOPD, COVHIV, COVCKD, COVDIAB, COVOBES, COVSICKLE, IMMUNOSUPPR, any_risk_factors)
+  
+  study_population <- study_population %>%
+    select(person_id, sex, study_entry_date, study_exit_date, ageband_at_date_vax_1, type_vax, Dose)
+  
+  study_population <- study_population[, Dose := as.character(Dose)]
+  
+  end_2020 <- ymd(20201231)
+  
+  vector_periods <- c(28, 60, 180, 365)
+  effective_starts <- c(0, head(vector_periods, -1))
+  
+  study_entry_names <- paste0("study_entry_date_", effective_starts)
+  study_exit_names <- paste0("study_exit_date_", vector_periods)
+  study_population <- study_population[, (study_entry_names) := lapply(effective_starts, `+`, study_entry_date)]
+  study_population <- study_population[, (study_exit_names) := lapply(vector_periods - 1, `+`, study_entry_date)]
+  setnames(study_population, c("study_entry_date", "study_exit_date"), c("orig_study_entry_date", "orig_study_exit_date"))
+  
+  study_population <- data.table::melt(study_population, measure = list(study_entry_names, study_exit_names),
+                                         variable.name = "Period", variable.factor = F,
+                                         value.name = c("study_entry_date", "study_exit_date"))
+  study_population[.(Period = as.character(seq_along(vector_periods)), to = as.character(vector_periods)), on = "Period", Period := i.to]
+  study_population <- study_population[study_entry_date <= orig_study_exit_date, ]
+  study_population <- study_population[study_exit_date > orig_study_exit_date, study_exit_date := orig_study_exit_date]
+  
+  study_population <- study_population[, c("orig_study_entry_date", "orig_study_exit_date") := NULL]
+  setnames(study_population, c("study_entry_date", "study_exit_date"), c("start_date_of_period", "end_date_of_period"))
+  
+  tempname <- paste0("D4_population_d_long", suffix[[subpop]])
+  assign(tempname, study_population)
+  save(tempname, file = paste0(diroutput, tempname,".RData"),list=tempname)
+  
+  rm(list=paste0("D4_population_d_long", suffix[[subpop]]))
+  rm(study_population)
+  
   
   # calculate correct fup_days
   D4_population_d[, fup_days := correct_difftime(study_exit_date_MIS_d, cohort_entry_date_MIS_d)]
@@ -168,7 +236,7 @@ for (subpop in subpopulations_non_empty) {
   rm(tempname)
   rm(D3_selection_criteria_d)
   if (this_datasource_has_subpopulations==T) rm(D4_population_d)
-  rm(events_ALL_OUTCOMES,study_population,outcomes_covid)
+  rm(events_ALL_OUTCOMES,outcomes_covid)
   
 }
 
