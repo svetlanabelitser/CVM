65b06f58d67de771af388c4d85c402008e98ba0b
Davide <davide.messina@ars.toscana.it>
2022-09-19 23:11:27 +0200

    correction for D_DIVERTICULITIS_AESI

--- p_parameters/06_variable_lists.R
+++ p_parameters/06_variable_lists.R
@@ -3,13 +3,19 @@
 VAR_codelist <- readxl::read_excel(File_variables_ALG_DP_ROC20_July22, sheet = "Variables")
 VAR_codelist <- as.data.table(VAR_codelist)
 
+# TODO remove before release?
+VAR_codelist <- VAR_codelist[Varname == "D_Diverticulitis_AESI", Varname := "D_DIVERTICULITIS_AESI"]
+
 OUTCOME_variables <- c(VAR_codelist[(AESI), Varname], DRUG_codelist[(AESI), Drug_proxie])
 
 # TODO remove before release?
-OUTCOME_variables <- OUTCOME_variables[OUTCOME_variables %not in% c("B_COAGDIS_AESI", "B_TTS_AESI")]
+OUTCOME_variables <- OUTCOME_variables[OUTCOME_variables %not in% c("B_COAGDIS_AESI")]
 
 CONTROL_variables <- VAR_codelist[(NEG), Varname]
+
+# TODO remove before release?
 CONTROL_variables <- CONTROL_variables[CONTROL_variables %not in% c("SO_CONJUNCTIVITIS_COV")]
+
 COV_variables <- c(VAR_codelist[(COV), Varname], DRUG_codelist[(COV), Drug_proxie])
 VACCINES_variable <- "COVID_VACCINES"
 
